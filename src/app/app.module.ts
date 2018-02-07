4e8bf6b78d0e02e4f5c6b42fc3722184845614fd
jstrating <jesper.strating@outlook.com>
2018-02-07 14:05:07 +0100

    Add auth service for login and user handling + Add auth guard service to handle routing guards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,25 +7,33 @@ import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
 
 import { environment } from '../environments/environment';
+import { AngularFireAuth } from 'angularfire2/auth';
 
+import {CoreModule} from './core/core.module';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './core/auth-guard.service';
+import { MatButtonModule } from '@angular/material';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent},
-  {path: 'profile', component: ProfileComponent}
+  {path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
+  {path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]},
+  {path: 'login', component: LoginComponent}
+
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ProfileComponent
+    ProfileComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -34,9 +42,11 @@ const routes: Routes = [
     RouterModule.forRoot(routes, {useHash: true}),
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
     AngularFireModule.initializeApp(environment.firebase, 'aurora-pwa'),
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    CoreModule,
+    MatButtonModule
   ],
-  providers: [],
+  providers: [AuthGuard, AngularFireAuth],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
